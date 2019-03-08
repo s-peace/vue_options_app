ac81c510dc0f51f38da01fc1e8a92e5bc3f034d3
s-peace <ssmim777@gmail.com>
2019-03-08 22:09:11 +0900

    animation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,13 +3,8 @@
     <h1>{{title}}</h1>
     <p>{{message}}</p>
     <hr>
-    <button v-on:click="doAction">{{btn}}</button>
-    <transition name="transit"
-      @before-enter="startAction"
-      @before-leave="startAction"
-      @after-enter="endAction"
-      @after-leave="endAction"
-    >
+    <button v-on:click="doAction">Show/Hide</button>
+    <transition name="transit">
       <p v-if="flg" class="trans">Transition</p>
     </transition>
   </div>
@@ -119,17 +114,18 @@ h1 {
   color: red;
 }
 .trans {
+  width: 150px;
   background-color: red;
   padding: 10px;
   font-size: 20px;
 }
-.transit-enter-active {
-  transition: opacity 2.0s;
-}
 .transit-leave-active {
-  transition: opacity 2.0s;
+  animation: anim 2.5s reverse;
 }
-.transit-enter {
+.transit-enter-active {
+  animation: anim 5.0s;
+}
+/* .transit-enter {
   opacity: 0;
 }
 .transit-enter-to {
@@ -140,5 +136,53 @@ h1 {
 }
 .taransit-leave-to {
   opacity: 0;
+}
+.tarnsit-enter, .transit-leave-to {
+  transform: translateX(200px)
+             translateY(-200px)
+             scale(5.0)
+             rotateY(360deg)
+             rotateX(360deg)
+             rotateZ(360deg);
+  opacity: 0.1;
+} */
+
+@keyframes anim {
+  0% {
+    font-size: 100px;
+    transform:  translateX(0px)
+                translateY(0px)
+                rotateZ(0deg);
+    opacity: 1.0;
+    background-color: #ddf;
+  }
+  25% {
+    transform:  translateX(250px)
+                translateY(0px)
+                rotateZ(0deg);
+    opacity: 1.0;
+    background-color: #fdd;
+  }
+  50% {
+    transform:  translateX(0px)
+                translateY(-100px)
+                rotateZ(540deg);
+    opacity: 1.0;
+    background-color: #dfd;
+  }
+  75% {
+    transform:  translateX(250px)
+                translateY(-100px)
+                rotateZ(540deg);
+    opacity: 1.0;
+    background-color: #fdf;
+  }
+  100% {
+    transform:  translateX(0px)
+                translateY(-200px)
+                rotateZ(1080deg);
+    opacity: 0;
+    background-color: #ffd;
+  }
 }
 </style>
